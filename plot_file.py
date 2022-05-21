171e490df25a622a73acfc45859f4c49684a5e66
Muzi12345632 <muziwandilenko@gmail.com>
2022-05-22 00:18:57 +0200

    plotfile for accuracy and loss

---
+++ plot_file.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from config import  history, model
+
+###PLot
+plt.figure(1)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['training', 'validation'])
+plt.title('loss')
+plt.xlabel('epoch')
+plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.legend(['training', 'validation'])
+plt.title('Accuracy')
+plt.xlabel('epoch')
+plt.show()
